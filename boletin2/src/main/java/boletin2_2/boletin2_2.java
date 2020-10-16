d38e6f9bb679e32a5424cce9aba0a5cb9bce88f9
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2020-10-16 09:36:33 +0200

    copia boletines

---
+++ boletin2/src/main/java/boletin2_2/boletin2_2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_2;
+
+/**
+ *
+ * @author cambes
+ */
+public class boletin2_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //area cuadrado
+    
+    float lado=3;
+    float areacuadrado ;
+    
+        areacuadrado=lado*lado;
+        System.out.println("El area de un cuadrado es = "+areacuadrado+"\n");
+        
+         /* libreria para poner exponentes
+        float lado2=3;
+        float areacuadrado2=(Float)Math.pow(lado2,2); // muestra area elevado exponente
+        
+        System.out.println("areacuadrado2 =***"+);
+        
+        */
+        
+        
+    }
+    
+}
---
+++ boletin2/src/main/java/boletin2_5/boletin2_5.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_5;
+
+import java.util.Scanner;
+/**
+ *
+ * @author cambes
+ */
+public class boletin2_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // programa que lea distancia en millas y la de en metros
+    
+       
+    
+    double metros;
+    double millasvalor=0.000621371;
+    double resultado2;
+        
+        
+
+     Scanner entrada3= new Scanner(System.in);                // funcion que solicitanumero
+        
+                
+        System.out.println("ingrese los metros =");    // solicita entrada de numero
+        metros=entrada3.nextDouble();                             // almacena dato metido en variable
+         
+        resultado2=(metros*millasvalor);
+        System.out.println("La distancia de ="+metros+"metros en millas es de ="+resultado2);
+       
+    
+        
+    }
+    
+}
