fd6778117654cff7844c5f751cb48f1968bbb64b
Gabrieldb <72223819+Gdominguezborines@users.noreply.github.com>
2020-10-26 10:32:36 +0100

    Add files via upload

---
+++ Boletin4/src/main/java/Libros/Libro.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Libros;
+
+/**
+ *
+ * @author cambes
+ */
+public class Libro {
+    
+ String titulo;
+ String autor;
+ int ano;
+ short numPaginas;
+ float valoracion;
+ 
+ 
+ public Libro(){
+                 
+     }
+
+
+      public Libro(String t, String Au, int A, Short Np, Float Va){      // constructor no por defecto 
+        titulo = t;                                 //  parametrizado
+        autor= Au;
+        ano= A;
+        numPaginas=Np;
+        valoracion=Va;    
+      
+      }
+
+
+
+ // metodos de acceso   getters  y setters
+   
+
+   
+  
+    
+      // getter
+    public String getTitulo(){
+        return titulo;
+    }
+     
+    public String getAutor(){
+         return autor;
+     }
+           
+    public int getAno(){
+         return ano;
+     }
+        
+    public short getNumPaginas(){
+         return numPaginas;
+     }
+      
+    public float getValoracion(){
+         return valoracion;
+     }
+    
+      //setter
+    public void setTitulo(String t){
+        
+        titulo=t;
+    }
+    
+    public void setValoracion(String Au ){
+         autor=Au;
+     }
+  
+    public void setAno(int A ){
+         ano =A;
+     }
+
+    public void setNumPaginas(short Np ){
+        numPaginas=Np;
+     }
+  
+    public void setValoracion(Float Va ){
+         valoracion=Va;
+     }
+     
+     
+     
+
+  //metodos
+        public void amosar (){
+        System.out.println("Titulo = "+titulo+"Autor= "+autor+" ano= "+ano+"Numero de paginas= "+numPaginas+"valoracion"+valoracion);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
